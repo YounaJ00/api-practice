a85d8a16819e027065ab3ca3767ea45fbb9594fb
younaj00 <you.na.joo0@gmail.com>
2025-04-02 11:59:07 +0900

    feat: 게시물 작성 시 새로고침 없이 데이터 처리

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,17 +1,46 @@
-const PostForm = () => {
+import axios from "axios";
+import { useState } from 'react';
+
+const PostForm = ({onPostSuccess}) => {
+
+  // 폼 제출시 페이지 새로고침 없이 js에서 데이터 처리
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8080/posts", {title, content});
+      // 초기화
+      setTitle("");
+      setContent("");
+      onPostSuccess(); // 게시물 추가 성공 후 home 상태 변경
+    } catch (error) {
+      console.error("게시물 작성 오류", error)  
+    }
+  };
+
     return (
       <div className="bg-white p-6 rounded-xl shadow-md w-1/3">
         <h2 className="text-lg font-bold mb-4">게시물 작성</h2>
         <input 
           type="text" 
           placeholder="제목을 입력하세요" 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           className="w-full border p-2 rounded-md mb-4"
         />
         <textarea 
           placeholder="내용을 입력하세요" 
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           className="w-full border p-2 rounded-md h-32 mb-4"
         ></textarea>
-        <button className="bg-black text-white px-4 py-2 rounded-md flex items-center">
+        <button 
+        className="bg-black text-white px-4 py-2 rounded-md flex items-center"
+        onClick={handleSubmit}
+        >
           ✏️ 작성하기
         </button>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
+import { useState } from "react";
 
 const Home = () => {
+
+  const [refreshKey, setRefreshKey] = useState(0); // 게시물 추가 시 변경될 상태
+  const handlePostSuccess = () => {
+    setRefreshKey((prev) => prev + 1);
+  };
+  
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-10">
       <div className="flex w-full max-w-6xl gap-6">
-        <PostList />
-        <PostForm />
+        <PostList refreshKey={refreshKey} />
+        <PostForm onPostSuccess={handlePostSuccess} />
       </div>
       <footer className="mt-10 text-gray-500 text-sm">
         © 2025 게시판 시스템. All rights reserved.
